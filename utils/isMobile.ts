e1b98d5e4be39ba697ad27a3ecf6817d232167a0
Lucas DePaola <lucasjdepaola@gmail.com>
2024-09-06 12:32:27 -0400

    fixed now

--- utils/isMobile.ts
+++ utils/isMobile.ts
@@ -2,8 +2,9 @@ import { userAgent } from "next/server";
 
 export const isMobile = () => {
     let mobileDimensions = false;
-    if(typeof window !== "undefined") {
+    if(typeof window !== "undefined" && typeof navigator !== undefined) {
         mobileDimensions = window.outerHeight >= 1080 && window.outerWidth <= 1080;
+        return /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || mobileDimensions;
     }
-    return /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || mobileDimensions;
+    return false; // handle later
 }
