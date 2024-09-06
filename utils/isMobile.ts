f30197cfa9425301be4a73ee61dfb5a504c32e06
Lucas DePaola <lucasjdepaola@gmail.com>
2024-09-06 12:29:46 -0400

    fixed build error

--- utils/isMobile.ts
+++ utils/isMobile.ts
@@ -1,6 +1,9 @@
 import { userAgent } from "next/server";
 
 export const isMobile = () => {
-    let mobileDimensions = window.outerHeight >= 1080 && window.outerWidth <= 1080;
+    let mobileDimensions = false;
+    if(typeof window !== "undefined") {
+        mobileDimensions = window.outerHeight >= 1080 && window.outerWidth <= 1080;
+    }
     return /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || mobileDimensions;
 }
