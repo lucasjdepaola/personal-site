0e6f6fe6ae75805a4ac906753edee3063e1c2c8d
Lucas DePaola <lucasjdepaola@gmail.com>
2024-09-10 18:29:50 -0400

    async error in posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,7 +6,7 @@ import { ColorfulLargeText, darkTheme, Description } from "@/components/mainpage
 import Posts from "@/components/posts/posts";
 import { Octokit } from "octokit";
 // .filter((path) => /\.mdx?$/.test(path));
-export default async function Home() {
+export default function Home() {
   const [darkMode, setDarkMode] = useState<boolean>(false);
   return (
     <>
@@ -25,28 +25,28 @@ export default async function Home() {
   );
 }
 
-const getServerSideProps = async () => {
-  const octokit = new Octokit({auth:"foobar"});
+// const getServerSideProps = async () => {
+//   const octokit = new Octokit({auth:"foobar"});
 
-  const { data } = await octokit.rest.repos.getContent({
-    owner: "lucasjdepaola",
-    repo: "mdx-content",
-    path: "",  // Folder where the MDX files are located
-  });
+//   const { data } = await octokit.rest.repos.getContent({
+//     owner: "lucasjdepaola",
+//     repo: "mdx-content",
+//     path: "",  // Folder where the MDX files are located
+//   });
 
-  let files;
-  if(Array.isArray(data)) {
-    files = data
-      .filter((file: any) => file.name.endsWith('.mdx'))
-      .map((file: any) => ({
-        name: file.name,
-        path: file.path,
-      }));
-  }
+//   let files;
+//   if(Array.isArray(data)) {
+//     files = data
+//       .filter((file: any) => file.name.endsWith('.mdx'))
+//       .map((file: any) => ({
+//         name: file.name,
+//         path: file.path,
+//       }));
+//   }
 
-  return {
-    props: {
-      files,
-    },
-  };
-};
+//   return {
+//     props: {
+//       files,
+//     },
+//   };
+// };
