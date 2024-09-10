218249d0b9a41bc58c4039e290ab3b7ae3814044
Lucas DePaola <lucasjdepaola@gmail.com>
2024-09-10 19:56:31 -0400

    updated posts

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,15 +1,8 @@
-import { MDXRemote } from "next-mdx-remote";
 import { compileMDX } from "next-mdx-remote/rsc";
 import { Octokit } from "octokit";
-import { serialize } from "v8";
-//https://github.com/vercel/next.js/discussions/58575
-//https://github.com/vercel/next.js/discussions/58575
-// /repos/{owner}/{repo}/contents/{path}
 // http://api.github.com/repos/{owner}/{repo}/contents/{path}
 // http://api.github.com/repos/lucasjdepaola/mdx-posts/contents/
-// ^^^^^^^^^^^ is a working link to get all the contents inside the git directory.
 // https://raw.githubusercontent.com/lucasjdepaola/mdx-posts/main/first.mdx
-// const POSTS_FOLDER = path.join(process.cwd(), "posts");
 
 interface FileContent {
   content: string;
@@ -20,7 +13,7 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 })
 
-const readMdxFile = async(slug: string): Promise<string> => {
+const readMdxFile = async(slug: string) => {
   let dt;
   try {
     const { data } = await octokit.rest.repos.getContent({
@@ -31,7 +24,7 @@ const readMdxFile = async(slug: string): Promise<string> => {
     dt = data;
   } catch(e) {
     console.log(e);
-    return "Not found.";
+    // return "Not found.";
   }
 
   const fd = dt as FileContent;
@@ -71,12 +64,7 @@ export default async function PostPage({
 }: {
   params: { slug: string };
 }) {
-  // const markdown = await readPostFile(params.slug);
   let markdown = await readMdxFile(params.slug);
-
-  if (!markdown) {
-    // notFound();
-  }
 
   const { content, frontmatter } = await compileMDX<{
     title: string;
@@ -84,24 +72,17 @@ export default async function PostPage({
     description?: string;
   }>({
     source: markdown,
-    options: { parseFrontmatter: true },
+    options: { parseFrontmatter: true, },
+    components: { // here you can edit the specific elements
+      h1: (props) => <h1 className="text-lg">{props.children}</h1>
+    }
   });
-
-
-  // do something with frontmatter, like set metadata or whatever
-  // figure this out intuitively, after implementing doc boiler plate
-
-  // return <>{content}</>;
-// export declare function MDXRemote<TScope, TFrontmatter>({ compiledSource, frontmatter, scope, components, lazy, }: MDXRemoteProps<TScope, TFrontmatter>): React.JSX.Element;
   return (
     <div>
-      {frontmatter.title} is the title
-      {frontmatter.date} is the date
       <div>
-        <>{content}</>
-        {/* {frontmatter.title} */}
+        {frontmatter.title}
       </div>
-      {/* {content} */}
+      {content}
     </div>
   )
 }
