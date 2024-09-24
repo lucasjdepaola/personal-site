29d80094ba6fcb0eaadd35cf515185a361ca2136
Lucas DePaola <lucasjdepaola@gmail.com>
2024-09-24 16:09:15 -0400

    bottom animation

--- components/os/osbottombar.tsx
+++ components/os/osbottombar.tsx
@@ -7,37 +7,44 @@ import { Position } from "./appsopened";
 // create an on hover context div for what the app name is
 export default function OSBottomBar(props: OpenedProps) {
     const [hoveredIndex, setHoveredIndex] = useState<number>(-1);
-    const mousePos = useRef<Position>({top: 0, left: 0});
+    const [mousePos, setMousePos] = useState<Position>({top: 0, left: 0});
     const [hasLeft, setHasLeft] = useState<boolean>(false);
     // animate given a width relative to the current cursor position
     return (
         <div className="fixed h-auto w-auto bottom-0 mb-3 text-black" style={{
             zIndex: "2",
-            transform: "translateX(calc(50vw - 50%))"
+            transform: "translateX(calc(50vw - 50%))",
             }}
-            onMouseMove={(e) => {mousePos.current = ({top: e.clientY, left: e.clientX})}}
+            onMouseMove={(e) => {setMousePos({top: e.clientY, left: e.clientX})}}
             onMouseLeave={() => setHasLeft(true)}
             onMouseEnter={() => setHasLeft(false)}
             >
-            <div className="flex bg-[#dadada] w-auto h-auto rounded-xl shadow-md">
+            <div className="flex bg-[#dadada] w-auto h-16 rounded-xl shadow-md">
                 {desktopicons.map((ico: DesktopIconLayout, i: number) => {
                     useEffect(() => {
                         setScalePos(scale());
-                    }, [mousePos.current])
+                    }, [mousePos, hoveredIndex])
                     const ref = useRef<HTMLDivElement | null>(null);
                     const [scalePos, setScalePos] = useState<number>(32);
                     const scale = () => {
                         if(ref.current) {
                             const rect = ref.current.getBoundingClientRect();
-                            const rel = mousePos.current.left - rect.width;
-                            return desktopicons.length - (Math.abs(i - hoveredIndex + 1)*10) + rel / 10;
+                            const circleCenterX = rect.left + rect.width / 2;
+                            const circleCenterY = rect.top + rect.height / 2;
+                            const distanceX = mousePos.left - circleCenterX;
+                            const distanceY = mousePos.top - circleCenterY;
+                            const distance = Math.hypot(distanceX, distanceY);
+                            const maxDistance = 300;
+                            const minScale = 16;
+                            const maxScale = 100;
+                            const scaleFactor = Math.max(minScale, maxScale - (distance / maxDistance) * (maxScale - minScale));
+                            return scaleFactor;
                         }
-                        return 1;
+                        return 32;
                     }
-                    return ( // child icon
-                        <div key={`bbico${i}`} className="relative flex justify-center items-center"
+                    return (
+                        <div key={`bbico${i}`} className="relative flex justify-center items-center p-1"
                         onMouseEnter={() => {
-                            // perform a more advanced animation here
                             setHoveredIndex(i);
                         }}
                         onMouseLeave={() => {setHoveredIndex(-1)}}
@@ -51,12 +58,8 @@ export default function OSBottomBar(props: OpenedProps) {
                                 </div>
                             )}
                             <img style={{
-                                // done via mutations
-                                width: hasLeft ? "32px" : scalePos + "px",
-                                height: hasLeft ? "32px" : scalePos + "px",
-                                // transform: `scale(${scale()})`,
-                                transition: "width .1s linear, height .1s linear"
-                                // transition: "transform .2s linear"
+                                width: hasLeft ? "40px" : scalePos + "px",
+                                height: hasLeft ? "40px" : scalePos + "px",
                             }} src={`${IMAGEPATH}${ico.appToOpen.image}`}></img>
                             {/* {props.openedApps.some(e => e.name === ico.appToOpen.name) && (
                                 <div className="absolute bottom-0 bg-black rounded-full left-1/2"></div>
